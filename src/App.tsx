c91617e22b49afc926f83ac480a4e194f348939e
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-28 09:15:56 +0200

    Add basic router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-function App() {
+import { Routes, Route } from "react-router-dom";
+import { Photos, Login, Register } from "./pages";
 
+function App() {
   return (
     <div className="App">
-      Hello
+      <Routes>
+        <Route path="/" element={<Photos />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
