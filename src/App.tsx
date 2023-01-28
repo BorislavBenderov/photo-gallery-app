f3482c1816b864b076d8aae64bab040d7a56d426
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-28 12:02:43 +0200

    Add App css

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Photos, Login, Register } from "./pages";
 
 function App() {
   return (
-    <div className="App">
+    <div className="max-w-7xl m-auto px-28 md:px-14 sm:px-2">
       <Routes>
         <Route path="/" element={<Photos />} />
         <Route path="/login" element={<Login />} />
