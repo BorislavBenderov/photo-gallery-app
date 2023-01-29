e50c756e22e30b060c26a2e819038709ca789d9d
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-29 11:56:01 +0200

    Export PhotoPage

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,4 @@
 export { Photos } from "./photos/Photos";
 export { Login } from "./login/Login";
 export { Register } from "./register/Register";
+export { PhotoPage } from "./photo-page/PhotoPage";
