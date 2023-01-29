17f386654f9d0fc2e0956bedf5b5def5a78adff5
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-29 09:52:58 +0200

    Add Photos page

--- src/pages/photos/Photos.tsx
+++ src/pages/photos/Photos.tsx
@@ -1,5 +1,57 @@
+import { Nav } from "../../components/nav/Nav";
+import { useEffect } from "react";
+import {
+  collection,
+  DocumentData,
+  onSnapshot,
+  query,
+  QuerySnapshot,
+  where,
+} from "firebase/firestore";
+import { db } from "../../firebaseConfig";
+import { Photo } from "../../components/photo/Photo";
+import { setPhotos } from "../../features/photo/photoSlice";
+import { useAppDispatch, useAppSelector } from "../../app/store";
+
 export const Photos = () => {
+  const { user } = useAppSelector((store) => store.user);
+  const { photos } = useAppSelector((store) => store.photo);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const q = query(
+      collection(db, "photos"),
+      where("ownerId", "==", user?.uid || "")
+    );
+
+    const unsibscribe = onSnapshot(
+      q,
+      (snapshot: QuerySnapshot<DocumentData>) => {
+        dispatch(
+          setPhotos(
+            snapshot.docs.map((item) => {
+              return { ...item.data(), id: item.id };
+            })
+          )
+        );
+      }
+    );
+
+    return () => {
+      unsibscribe();
+    };
+  }, [dispatch, user]);
+
   return (
-    <div>Photos</div>
-  )
-}
+    <div>
+      <div className="my-[25px]">
+        <Nav />
+      </div>
+      <div className="flex flex-wrap gap-4">
+        {photos.map((photo) => (
+          <Photo key={photo.id} photo={photo} />
+        ))}
+      </div>
+    </div>
+  );
+};
