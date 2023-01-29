25f23f9c246eb701f320d004d4bf459d2bae4677
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-29 09:52:18 +0200

    Add onAuthStateChanged

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
 import { Routes, Route } from "react-router-dom";
 import { Photos, Login, Register } from "./pages";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { login, logout } from "./features/user/userSlice";
+import { auth } from "./firebaseConfig";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch(
+          login({
+            email: user.email,
+            uid: user.uid,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+
+    return () => {
+      unsub();
+    };
+  }, [dispatch]);
+
   return (
-    <div className="max-w-7xl m-auto px-28 md:px-14 sm:px-2">
+    <div className="max-w-7xl px-[20px] mx-auto">
       <Routes>
         <Route path="/" element={<Photos />} />
         <Route path="/login" element={<Login />} />
