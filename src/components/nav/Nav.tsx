ad1d60642bbd382cc4e435ba6d46d5d12a3c8ce2
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-02-02 23:39:13 +0200

    Render user email

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,10 +1,13 @@
+import { useAppSelector } from "../../app/store";
 import { Logout } from "../logout/Logout";
 import { UploadPhoto } from "../upload-photo/UploadPhoto";
 
 export const Nav = () => {
+  const { user } = useAppSelector((store) => store.user);
   return (
     <nav>
       <ul className="flex justify-end gap-4">
+        <li>Hello, {user?.email}</li>
         <li>
           <UploadPhoto />
         </li>
